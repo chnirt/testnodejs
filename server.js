d6ab0f7bdd58d9ce792cd9324eea6adc73ebf130
Chin <35022967+chinnghiemtuc@users.noreply.github.com>
2019-03-11 13:59:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
 // 	res.render('index');
-	JSON.stringify({ Hello: ‘World’})
+	res.send(JSON.stringify({ Hello: ‘World’}));
 });
 
 app.listen(port, function() {
